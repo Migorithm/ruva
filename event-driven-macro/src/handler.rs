7b97859275a51e594b2b774d69e40a33521ca59f
Migorithm <saka90030@gmail.com>
2023-10-04 01:40:15 +0900

    Removal of context attribute

--- event-driven-macro/src/handler.rs
+++ event-driven-macro/src/handler.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{punctuated::Punctuated, token::Comma, FnArg, Ident, ItemFn, Pat, PatIdent, PatType, Path, ReturnType, Signature, Type, TypePath};
+use syn::{punctuated::Punctuated, token::Comma, FnArg, Ident, ItemFn, Meta, Pat, PatIdent, PatType, Path, ReturnType, Signature, Type, TypePath};
 
 pub fn parse_handler(ast: ItemFn) -> TokenStream {
 	const OUTPUT_TYPE_NOT_VALID: &str = "#[handler] fn must have valid output type";
@@ -14,7 +14,7 @@ pub fn parse_handler(ast: ItemFn) -> TokenStream {
 		},
 		block,
 		..
-	} = ast.clone()
+	} = ast
 	else {
 		panic!("{}", OUTPUT_TYPE_NOT_VALID)
 	};
@@ -24,15 +24,20 @@ pub fn parse_handler(ast: ItemFn) -> TokenStream {
 	}
 
 	// TODO Check if the first argument is of either Command or Message
+
 	let message = inputs.first().unwrap();
-	let args = inputs.iter().skip(1).cloned().collect::<Punctuated<FnArg, Comma>>();
-	// extract_dependency(args);
+	let mut args = inputs.iter().skip(1).cloned().collect::<Punctuated<FnArg, Comma>>();
+	remove_context_attribute(&mut args);
 
 	// Get idents. This will be passed to inner(or redefined) function
 	let idents = get_puntuated_idents(inputs.clone());
 
 	// injectables
 	let injectables = take_injectables(inputs.clone());
+
+	// TODO case1 - context required
+	// TODO case2 - context-injection required
+	// TODO case3 - context not required
 
 	quote!(
 		pub #asyncness fn #ident #generics(#message,context:event_driven_library::prelude::AtomicContextManager)-> #var{
@@ -72,7 +77,20 @@ fn take_injectables(inputs: Punctuated<FnArg, Comma>) -> Vec<proc_macro2::TokenS
 			// TODO get attributes on function parameter to sort out context, context-injectable and context-agnostic-injectables
 			if let FnArg::Typed(PatType { pat, attrs, .. }) = i {
 				match *pat {
-					Pat::Ident(PatIdent { ident, .. }) => Some(quote!(#ident)),
+					Pat::Ident(PatIdent { ident, .. }) => {
+						let mut is_context = false;
+
+						attrs.first().map(|attr| match &attr.meta {
+							Meta::Path(Path { segments, .. }) => segments.first().map(|seg| is_context = seg.ident == *"context"),
+							_ => panic!("Not allowed!"),
+						});
+
+						if !is_context {
+							Some(quote!(#ident))
+						} else {
+							None
+						}
+					}
 					_ => panic!("Not Allowed!"),
 				}
 			} else {
@@ -80,4 +98,19 @@ fn take_injectables(inputs: Punctuated<FnArg, Comma>) -> Vec<proc_macro2::TokenS
 			}
 		})
 		.collect::<Vec<_>>()
+}
+
+fn remove_context_attribute(args: &mut Punctuated<FnArg, Comma>) -> Option<String> {
+	for arg in args.into_iter() {
+		if let FnArg::Typed(PatType { attrs, .. }) = arg {
+			let finded_context = attrs.iter_mut().find(|x| x.path().is_ident("context"));
+			if finded_context.is_none() {
+				continue;
+			}
+			let result = finded_context.unwrap().path().get_ident().unwrap().to_string();
+			attrs.retain(|x| !x.path().is_ident("context"));
+			return Some(result);
+		} // #[context]
+	}
+	None
 }
