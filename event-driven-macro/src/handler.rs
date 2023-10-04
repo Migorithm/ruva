0bb6205a872646f6e355cd448ad8ce51b6c72e9e
Migo <migo@Migos-MacBook-Pro.local>
2023-10-04 19:11:17 +0900

    TODO finished

--- event-driven-macro/src/handler.rs
+++ event-driven-macro/src/handler.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{punctuated::Punctuated, token::Comma, FnArg, Ident, ItemFn, Meta, Pat, PatIdent, PatType, Path, ReturnType, Signature, Type, TypePath};
+use syn::{punctuated::Punctuated, token::Comma, FnArg, Ident, ItemFn, Meta, Pat, PatIdent, PatType, Path, ReturnType, Signature};
 
 pub fn parse_handler(ast: ItemFn) -> TokenStream {
 	const OUTPUT_TYPE_NOT_VALID: &str = "#[handler] fn must have valid output type";
@@ -8,9 +8,10 @@ pub fn parse_handler(ast: ItemFn) -> TokenStream {
 			ident,
 			output: ReturnType::Type(_, var),
 			inputs,
-			generics,
+			mut generics,
 			asyncness,
 			..
+			
 		},
 		block,
 		..
@@ -26,30 +27,35 @@ pub fn parse_handler(ast: ItemFn) -> TokenStream {
 	// TODO Check if the first argument is of either Command or Message
 
 	let message = inputs.first().unwrap();
+
 	let mut args = inputs.iter().skip(1).cloned().collect::<Punctuated<FnArg, Comma>>();
-	remove_context_attribute(&mut args);
+	let flagged_args: Vec<(FnArg, bool)> = flag_context(&mut args);
 
 	// Get idents. This will be passed to inner(or redefined) function
 	let idents = get_puntuated_idents(inputs.clone());
 
 	// injectables
-	let injectables = take_injectables(inputs.clone());
+	let injectables = take_injectables(flagged_args);
 
-	// TODO case1 - context required
-	// TODO case2 - context-injection required
-	// TODO case3 - context not required
+
+
+	let generic_where = &generics.where_clause;
 
 	quote!(
-		pub #asyncness fn #ident #generics(#message,context:event_driven_library::prelude::AtomicContextManager)-> #var{
-			#asyncness fn inner(#message,#args)->#var{
+
+		pub #asyncness fn #ident #generics (#message,context:event_driven_library::prelude::AtomicContextManager)-> #var #generic_where {
+
+
+			#asyncness fn inner #generics (#message,#args)->#var #generic_where{
 				#block
 			};
 			let dependency= crate::dependencies::dependency();
 			#(
-				let #injectables = dependency.#injectables();
+				#injectables
 			)*
+
 			inner(#idents).await
-		}
+			}
 	)
 	.into()
 }
@@ -69,48 +75,75 @@ fn get_puntuated_idents(inputs: Punctuated<FnArg, Comma>) -> Punctuated<Ident, C
 		})
 		.collect()
 }
-fn take_injectables(inputs: Punctuated<FnArg, Comma>) -> Vec<proc_macro2::TokenStream> {
+
+fn flag_context(args: &mut Punctuated<FnArg, Comma>) -> Vec<(FnArg, bool)> {
+	let mut container = vec![];
+
+	for arg in args.into_iter() {
+		let mut context_injection_required = false;
+
+		if let FnArg::Typed(PatType { ref mut attrs, .. }) = arg {
+			// * Take initial length
+			let init_len = attrs.len();
+			attrs.retain(|x| !x.path().is_ident("context"));
+
+			// TODO if attribute other than `context` is passed, cause panic
+
+			// * If initial length is not the same after ratain method is applied, it means 'context' attribute has been idenfitied
+			if init_len != attrs.len() {
+				context_injection_required = true;
+			}
+		}
+		container.push((arg.clone(), context_injection_required))
+	}
+	container
+}
+
+fn take_injectables(inputs: Vec<(FnArg, bool)>) -> Vec<proc_macro2::TokenStream> {
 	inputs
 		.into_iter()
-		.skip(1)
 		.filter_map(|i| {
 			// TODO get attributes on function parameter to sort out context, context-injectable and context-agnostic-injectables
-			if let FnArg::Typed(PatType { pat, attrs, .. }) = i {
-				match *pat {
-					Pat::Ident(PatIdent { ident, .. }) => {
-						let mut is_context = false;
 
-						attrs.first().map(|attr| match &attr.meta {
-							Meta::Path(Path { segments, .. }) => segments.first().map(|seg| is_context = seg.ident == *"context"),
-							_ => panic!("Not allowed!"),
-						});
+			match i {
+				(FnArg::Typed(PatType { pat, .. }), false) => match *pat {
+					Pat::Ident(PatIdent { ident, .. }) => Some(quote!(
 
-						if !is_context {
-							Some(quote!(#ident))
-						} else {
-							None
-						}
-					}
+						let #ident = dependency.#ident();
+					)),
 					_ => panic!("Not Allowed!"),
-				}
-			} else {
-				None
+				},
+				(FnArg::Typed(PatType { pat, .. }), true) => match *pat {
+					Pat::Ident(PatIdent { ident, .. }) => Some(quote!(
+
+						let #ident = dependency.#ident(context).await;
+					)),
+					_ => panic!("Not Allowed!"),
+				},
+				_ => None,
 			}
 		})
 		.collect::<Vec<_>>()
 }
 
-fn remove_context_attribute(args: &mut Punctuated<FnArg, Comma>) -> Option<String> {
-	for arg in args.into_iter() {
-		if let FnArg::Typed(PatType { attrs, .. }) = arg {
-			let finded_context = attrs.iter_mut().find(|x| x.path().is_ident("context"));
-			if finded_context.is_none() {
-				continue;
-			}
-			let result = finded_context.unwrap().path().get_ident().unwrap().to_string();
-			attrs.retain(|x| !x.path().is_ident("context"));
-			return Some(result);
-		} // #[context]
-	}
-	None
-}
+
+
+// #[message_handler]
+//	pub async fn create_task2<R: TRepository<E, TaskAggregate>, E: Executor + Sync + Send>(
+//		cmd: CreateTask,
+//		#[context] task_uow2: UnitOfWork<R, E, TaskAggregate>,
+//	) -> Result<ServiceResponse, ServiceError>
+//	where
+//		ServiceOutBox: IOutBox<TExecutor>,
+//		ServiceOutBox: IOutBox<E>,
+//	{
+//		let mut uow = task_uow2;
+//
+//		let mut task_aggregate = TaskAggregate::default();
+//		task_aggregate.create_task(cmd);
+//		let res = uow.repository().add(&mut task_aggregate).await?;
+//
+//		uow.commit::<ServiceOutBox>().await?;
+//		Ok(res.into())
+//	}
+
