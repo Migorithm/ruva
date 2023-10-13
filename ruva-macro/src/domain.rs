b759f90649a1a5fa48a7e3ecc0e78a686745917b
Migo <migo@Migos-MacBook-Pro.local>
2023-10-13 11:40:17 +0900

    aggregate: change from `Derive` to `Attribute` macro so it can add default fields

--- ruva-macro/src/domain.rs
+++ ruva-macro/src/domain.rs
@@ -27,6 +27,62 @@ pub(crate) fn render_aggregate_token(ast: &DeriveInput) -> TokenStream {
 	.into()
 }
 
+pub(crate) fn render_aggregate(input: TokenStream) -> TokenStream {
+	let mut ast = parse_macro_input!(input as DeriveInput);
+	let name = &ast.ident;
+	let crates = locate_crate_on_derive_macro(&ast);
+
+	if let syn::Data::Struct(DataStruct {
+		fields: syn::Fields::Named(ref mut fields),
+		..
+	}) = &mut ast.data
+	{
+		fields.named.extend([
+			syn::Field::parse_named
+				.parse2(quote! {
+				   #[serde(skip_deserializing, skip_serializing)]
+				   pub(crate) is_existing: bool
+				})
+				.expect("is_existing field not injectable! Perhaps it's duplicated?"),
+			syn::Field::parse_named
+				.parse2(quote! {
+				   #[serde(skip_deserializing, skip_serializing)]
+				   pub(crate) events: ::std::collections::VecDeque<::std::boxed::Box<dyn #crates::prelude::Message>>
+
+				})
+				.expect("events field not injectable! Perhaps it's duplicated?"),
+			syn::Field::parse_named
+				.parse2(quote! {
+				   pub(crate) version: i32
+				})
+				.expect("version field not Injectable! Perhaps it's duplicated?"),
+		]);
+	} else {
+		panic!("[entity] can be attached only to struct")
+	}
+	let setters = get_setters(&ast.data);
+
+	quote!(
+		#ast
+		impl #crates::prelude::Aggregate for #name{
+			fn events(&self) -> &::std::collections::VecDeque<Box<dyn #crates::prelude::Message>> {
+				&self.events
+			}
+			fn take_events(&mut self) -> ::std::collections::VecDeque<Box<dyn #crates::prelude::Message>> {
+				::std::mem::take(&mut self.events)
+			}
+			fn raise_event(&mut self, event: Box<dyn #crates::prelude::Message>) {
+				self.events.push_back(event)
+			}
+		}
+
+		impl #name{
+			#setters
+		}
+	)
+	.into()
+}
+
 pub(crate) fn render_entity_token(input: TokenStream) -> TokenStream {
 	let mut ast = parse_macro_input!(input as DeriveInput);
 	let name = &ast.ident;
