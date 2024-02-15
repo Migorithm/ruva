5a8f335b917ec43597f98e8599824c3b2223ab3a
Migorithm <saka90030@gmail.com>
2024-02-16 01:28:16 +0900

    sqlx version up

--- ruva-core/src/rdb/executor.rs
+++ ruva-core/src/rdb/executor.rs
@@ -1,6 +1,10 @@
 use crate::prelude::{BaseError, TUnitOfWork};
 
-use sqlx::{pool::PoolOptions, postgres::PgConnectOptions, postgres::PgPool, ConnectOptions, Postgres, Transaction};
+use sqlx::{
+	pool::PoolOptions,
+	postgres::{PgConnectOptions, PgPool},
+	ConnectOptions, PgConnection, Postgres, Transaction,
+};
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -18,7 +22,7 @@ impl SQLExecutor {
 		}))
 	}
 
-	pub fn transaction(&mut self) -> &mut Transaction<'static, Postgres> {
+	pub fn transaction(&mut self) -> &mut PgConnection {
 		match self.transaction.as_mut() {
 			Some(trx) => trx,
 			None => panic!("Transaction Has Not Begun!"),
@@ -73,11 +77,10 @@ pub fn connection_pool() -> &'static PgPool {
 			#[tokio::main]
 			async fn get_connection_pool() -> PgPool {
 				let url = &std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-				let mut opts: PgConnectOptions = url.parse().unwrap();
-				opts.disable_statement_logging();
+
 				PoolOptions::new()
 					.max_connections(100)
-					.connect_with(opts)
+					.connect_with(url.parse::<PgConnectOptions>().unwrap().disable_statement_logging())
 					.await
 					.map_err(|err| BaseError::DatabaseError(Box::new(err)))
 					.unwrap()
