90aef1ae203a2945076dbcf297f4a891779d2cbb
Migo <migo@Migos-MacBook-Pro.local>
2024-08-06 16:33:01 +0900

    feature: simplified command service trait

--- ruva-core/src/bus_components/handler/mod.rs
+++ ruva-core/src/bus_components/handler/mod.rs
@@ -1,13 +1,47 @@
+//! ### example
+//! ```rust,no_run
+//! impl<R, C> TCommandService<ServiceResponse, ServiceError> for UnitOfWorkHandler<C, R>
+//! where
+//!     R: TRepository + TUnitOfWork,
+//!     C: TCommand,
+//!     Self: TRunCommand,
+//! {
+//!     async fn execute(&mut self) -> Result<ServiceResponse> {
+//!         self.begin().await?;
+//!
+//!         match self.run_command().await {
+//!             Ok(val) => {
+//!                 self.commit().await?;
+//!                 self.close().await;
+//!
+//!                 Ok(val)
+//!             }
+//!             Err(err) => {
+//!                 self.rollback().await?;
+//!                 self.close().await;
+//!                 if let ServiceError::StopSentinelWithEvent(event) = err {
+//!                     self.set_events(vec![event.clone()].into());
+//!                     self.process_internal_events().await?;
+//!                     self.process_external_events().await?;
+//!                     Err(ServiceError::StopSentinelWithEvent(event))
+//!                 } else {
+//!                     Err(err)
+//!                 }
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
+
 pub mod event;
 
-use crate::prelude::{ApplicationError, ApplicationResponse, TCommand};
+use crate::prelude::{ApplicationError, ApplicationResponse};
 pub use event::*;
 
-pub trait TCommandService<R, E, C>: Send + Sync
+pub trait TCommandService<R, E>: Send + Sync
 where
 	R: ApplicationResponse,
 	E: ApplicationError,
-	C: TCommand,
 {
-	fn execute(&mut self, cmd: C) -> impl std::future::Future<Output = Result<R, E>> + Send;
+	fn execute(&mut self) -> impl std::future::Future<Output = Result<R, E>> + Send;
 }
