6bc555ef593dafddd2675f303a1e692bc15a074a
Migo <migo@Migos-MacBook-Pro.local>
2024-08-13 11:53:03 +0900

    fix: uncomment tests

--- tests/commands.rs
+++ tests/commands.rs
@@ -1,94 +1,94 @@
 use ruva::*;
 use ruva_macro::into_command;
 
-// #[test]
-// fn test_into_command_body_command_specified() {
-// 	#[into_command(body(Serialize), command(Serialize, serde::Deserialize))]
-// 	struct SomeCommand {
-// 		#[required_input]
-// 		id: i32,
-// 		name: String,
-// 		foo: i32,
-// 	}
+#[test]
+fn test_into_command_body_command_specified() {
+	#[into_command(body(Serialize), command(Serialize, serde::Deserialize))]
+	struct SomeCommand {
+		#[required_input]
+		id: i32,
+		name: String,
+		foo: i32,
+	}
 
-// 	let command = SomeCommandBody { name: "migo".into(), foo: 2 };
-// 	let serilaized = serde_json::to_string(&command).unwrap();
-// 	let deserialized: SomeCommandBody = serde_json::from_str::<SomeCommandBody>(&serilaized).unwrap();
-// 	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2 }".to_string());
+	let command = SomeCommandBody { name: "migo".into(), foo: 2 };
+	let serilaized = serde_json::to_string(&command).unwrap();
+	let deserialized: SomeCommandBody = serde_json::from_str::<SomeCommandBody>(&serilaized).unwrap();
+	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2 }".to_string());
 
-// 	let command2 = deserialized.into_command(1);
-// 	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2 }".to_string());
-// 	let _ = serde_json::from_str::<SomeCommand>(&serde_json::to_string(&command2).unwrap()).unwrap();
-// }
+	let command2 = deserialized.into_command(1);
+	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2 }".to_string());
+	let _ = serde_json::from_str::<SomeCommand>(&serde_json::to_string(&command2).unwrap()).unwrap();
+}
 
-// #[test]
-// fn test_into_command_only_body_specified() {
-// 	#[into_command(body(Serialize))]
-// 	struct SomeCommand {
-// 		#[required_input]
-// 		id: i32,
-// 		name: String,
-// 		foo: i32,
-// 	}
+#[test]
+fn test_into_command_only_body_specified() {
+	#[into_command(body(Serialize))]
+	struct SomeCommand {
+		#[required_input]
+		id: i32,
+		name: String,
+		foo: i32,
+	}
 
-// 	let command = SomeCommandBody { name: "migo".into(), foo: 2 };
-// 	let serilaized = serde_json::to_string(&command).unwrap();
+	let command = SomeCommandBody { name: "migo".into(), foo: 2 };
+	let serilaized = serde_json::to_string(&command).unwrap();
 
-// 	let deserialized: SomeCommandBody = serde_json::from_str::<SomeCommandBody>(&serilaized).unwrap();
-// 	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2 }".to_string());
+	let deserialized: SomeCommandBody = serde_json::from_str::<SomeCommandBody>(&serilaized).unwrap();
+	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2 }".to_string());
 
-// 	let command2: SomeCommand = deserialized.into_command(1);
-// 	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2 }".to_string());
-// }
+	let command2: SomeCommand = deserialized.into_command(1);
+	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2 }".to_string());
+}
 
-// #[test]
-// fn test_into_command_nothing_specified() {
-// 	#[into_command]
-// 	struct SomeCommand {
-// 		#[required_input]
-// 		id: i32,
-// 		name: String,
-// 		foo: i32,
-// 	}
+#[test]
+fn test_into_command_nothing_specified() {
+	#[into_command]
+	struct SomeCommand {
+		#[required_input]
+		id: i32,
+		name: String,
+		foo: i32,
+	}
 
-// 	let deserialized: SomeCommandBody = serde_json::from_str("{\"name\":\"migo\",\"foo\":2}").unwrap();
-// 	let commnand = deserialized.into_command(1);
-// 	let _serialized = serde_json::to_string(&commnand).unwrap();
-// }
+	let deserialized: SomeCommandBody = serde_json::from_str("{\"name\":\"migo\",\"foo\":2}").unwrap();
+	let commnand = deserialized.into_command(1);
+	let _serialized = serde_json::to_string(&commnand).unwrap();
+}
 
-// #[test]
-// fn test_into_command_with_generic() {
-// 	// Even without the `Sync`, `Send` and `Debug` contraint on generic type T, it's still valid.
+#[test]
+fn test_into_command_with_generic() {
+	// Even without the `Sync`, `Send` and `Debug` contraint on generic type T, it's still valid.
 
-// 	#[into_command(body(Serialize))]
-// 	struct SomeCommand<T: Serialize> {
-// 		#[required_input]
-// 		id: i32,
-// 		name: String,
-// 		foo: i32,
-// 		t_field: T,
-// 	}
+	#[into_command(body(Serialize))]
+	struct SomeCommand<T: Serialize> {
+		#[required_input]
+		id: i32,
+		name: String,
+		foo: i32,
+		t_field: T,
+	}
 
-// 	let command = SomeCommandBody::<i32> {
-// 		name: "migo".into(),
-// 		foo: 2,
-// 		t_field: 1,
-// 	};
-// 	let serilaized = serde_json::to_string(&command).unwrap();
-// 	let deserialized: SomeCommandBody<i32> = serde_json::from_str::<SomeCommandBody<i32>>(&serilaized).unwrap();
-// 	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2, t_field: 1 }".to_string());
+	let command = SomeCommandBody::<i32> {
+		name: "migo".into(),
+		foo: 2,
+		t_field: 1,
+	};
+	let serilaized = serde_json::to_string(&command).unwrap();
+	let deserialized: SomeCommandBody<i32> = serde_json::from_str::<SomeCommandBody<i32>>(&serilaized).unwrap();
+	assert_eq!(format!("{:?}", deserialized), "SomeCommandBody { name: \"migo\", foo: 2, t_field: 1 }".to_string());
 
-// 	let command2 = deserialized.into_command(1);
-// 	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2, t_field: 1 }".to_string());
-// }
+	let command2 = deserialized.into_command(1);
+	assert_eq!(format!("{:?}", command2), "SomeCommand { id: 1, name: \"migo\", foo: 2, t_field: 1 }".to_string());
+}
 
-// #[test]
-// fn test_unit_command() {
-// 	#[into_command]
-// 	struct UnitCommand;
-// 	fn fn_accept_t_command<T: ruva::TCommand>(_: T) {}
-// 	fn_accept_t_command(UnitCommand);
-// }
+#[test]
+fn test_unit_command() {
+	#[into_command]
+	struct UnitCommand;
+	fn fn_accept_t_command<T: ruva::TCommand>(_: T) {}
+	fn_accept_t_command(UnitCommand);
+}
 
 #[test]
 fn test_into_command_with_tevent() {
